b32892c75daf329ea263517630741963a7c14f23
nel023 <germanronelportfolio@gmail.com>
2023-09-29 23:34:38 +0800

    Start of progress with C#.

---
+++ TestProject/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Random Dice = new Random();
+int zeroRoll = Dice.Next(2,5);
+Console.WriteLine(zeroRoll + "first");
+
+int roll = new Random().Next(1,7);
+Console.WriteLine(roll);
